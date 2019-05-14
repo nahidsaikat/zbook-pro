cc80c51ac99b0894ec5edc1ee0797abb4ae081aa
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-14 06:25:28 +0600

    Changing module name from zbook_pro to zbook

--- zbook/settings.py
+++ zbook/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'zbook_pro.urls'
+ROOT_URLCONF = 'zbook.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'zbook_pro.wsgi.application'
+WSGI_APPLICATION = 'zbook.wsgi.application'
 
 
 # Database
