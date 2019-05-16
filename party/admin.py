d532a0b4d0bd032f3ccc0ce3cca3c87ff024bba6
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-16 21:20:39 +0600

    <7> Add admin classes for customer and vendor

--- party/admin.py
+++ party/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .forms import CustomerForm, VendorForm
+from .models import Customer, Vendor
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    form = CustomerForm
+    list_display = ('name', 'phone', 'email', 'company_name', 'type')
+    list_filter = ('name', 'type')
+    search_fields = ('name', 'type')
+    ordering = ('id', )
+
+
+class VendorAdmin(admin.ModelAdmin):
+    form = VendorForm
+    list_display = ('name', 'phone', 'email', 'company_name', 'type')
+    list_filter = ('name', 'type')
+    search_fields = ('name', 'type')
+    ordering = ('id', )
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Vendor, VendorAdmin)
