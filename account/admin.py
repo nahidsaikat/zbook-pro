97e8e01be2806564afb712d736d9966e7a56bbe9
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-16 19:59:44 +0600

    <4> Add admin class for account and account type

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import AccountSubType, Account
+from .forms import AccountSubTypeForm, AccountForm
 
 
-# Register your models here.
-admin.site.register(AccountSubType)
-admin.site.register(Account)
+class AccountSubTypeAdmin(admin.ModelAdmin):
+    form = AccountSubTypeForm
+    list_display = ('name', 'type', 'order')
+    list_filter = ('name', 'type')
+    search_fields = ('name', 'type')
+    ordering = ('type', )
+
+
+class AccountAdmin(admin.ModelAdmin):
+    form = AccountForm
+    list_display = ('name', 'number', 'type', 'sub_type', 'depth', 'entry_date')
+    list_filter = ('name', 'type', 'sub_type', 'entry_date')
+    search_fields = ('name', 'type', 'sub_type', 'entry_date')
+    ordering = ('entry_date', 'type', 'sub_type')
+
+
+admin.site.register(AccountSubType, AccountSubTypeAdmin)
+admin.site.register(Account, AccountAdmin)
