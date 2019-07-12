669a04e217a92a4dfae4a31690d7e350706f8017
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-12 22:12:05 +0600

    Edit test for account sub type

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -16,12 +16,12 @@ class TestAccountSubType:
         sub_type.save()
 
         query = AccountSubType.objects.all()
-        saved_object = query.first()
+        saved_obj = query.first()
 
         assert query.count() == 1
-        assert saved_object.name == sub_type.name
-        assert saved_object.type == sub_type.type
-        assert saved_object.order == sub_type.order
+        assert saved_obj.name == sub_type.name
+        assert saved_obj.type == sub_type.type
+        assert saved_obj.order == sub_type.order
 
     @pytest.mark.django_db
     def test_count(self):
@@ -31,3 +31,17 @@ class TestAccountSubType:
         query = AccountSubType.objects.all()
 
         assert query.count() == 2
+
+    @pytest.mark.django_db
+    def test_edit(self):
+        sub_type = AccountSubType.objects.create(name=fake.name(), type=AccountType.Asset, order=0)
+        sub_type.save()
+        new_name = 'new name'
+        sub_type.name = new_name
+        sub_type.order = 1
+        sub_type.save()
+
+        saved_obj = AccountSubType.objects.first()
+
+        assert saved_obj.name == new_name
+        assert saved_obj.order == 1
