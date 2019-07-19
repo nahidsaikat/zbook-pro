d9777a6eea5e6bbcc1f81e1673a53839931d9f45
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-20 05:41:51 +0600

    Add test for account subtype detail update api views

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -65,3 +65,67 @@ class TestAccountSubTypeListCreateAPIView:
         response = client.get(self.url)
 
         assert response.status_code == 401
+
+
+class TestAccountSubTypeRetrieveUpdateAPIView:
+
+    def test_update_name(self, auth_client):
+        subtype = AccountSubTypeFactory()
+        name = fake.name()
+        data = factory.build(dict, FACTORY_CLASS=AccountSubTypeFactory, name=name)
+
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+        response = auth_client.patch(url, data)
+
+        assert response.status_code == 200
+        assert response.data.get('name') == name
+
+    def test_update_type(self, auth_client):
+        subtype = AccountSubTypeFactory()
+        _type = FuzzyChoice(choices=AccountType.values.keys()).fuzz()
+        data = factory.build(dict, FACTORY_CLASS=AccountSubTypeFactory, type=_type)
+
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+        response = auth_client.patch(url, data)
+
+        assert response.status_code == 200
+        assert response.data.get('type') == _type
+
+    def test_update_order(self, auth_client):
+        subtype = AccountSubTypeFactory()
+        order = random.randint(0, 10)
+        data = factory.build(dict, FACTORY_CLASS=AccountSubTypeFactory, order=order)
+
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+        response = auth_client.patch(url, data)
+
+        assert response.status_code == 200
+        assert response.data.get('order') == order
+
+    def test_update_unauthorize(self, client, db):
+        subtype = AccountSubTypeFactory()
+        data = factory.build(dict, FACTORY_CLASS=AccountSubTypeFactory)
+
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+        response = client.patch(url, data)
+
+        assert response.status_code == 401
+
+    def test_get_subtype(self, auth_client):
+        subtype = AccountSubTypeFactory()
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+
+        response = auth_client.get(url)
+
+        assert response.status_code == 200
+        assert response.data.get('name') == subtype.name
+        assert response.data.get('type') == subtype.type
+        assert response.data.get('order') == subtype.order
+
+    def test_get_subtype_unauthorize(self, client, db):
+        subtype = AccountSubTypeFactory()
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+
+        response = client.get(url)
+
+        assert response.status_code == 401
