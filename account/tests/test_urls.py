199a64604b103891fd4192c80dc1522b381cfaa5
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-20 05:29:58 +0600

    Add test for account subtype update detail url

--- account/tests/test_urls.py
+++ account/tests/test_urls.py
@@ -1,4 +1,5 @@
 from django.urls import reverse, resolve
+from .factory import AccountSubTypeFactory
 
 
 class TestAccountSubTypeUrls:
@@ -17,3 +18,20 @@ class TestAccountSubTypeUrls:
         assert resolver.view_name == 'account:subtype:list-create'
         assert resolver.namespace == 'account:subtype'
         assert resolver.func.__name__ == 'AccountSubTypeListCreateAPIView'
+
+    def test_detail_update_url_reverse(self, db):
+        subtype = AccountSubTypeFactory()
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+        assert url == '/api/v1/account/subtype/1/'
+
+    def test_detail_update_url_resolve(self, db):
+        subtype = AccountSubTypeFactory()
+        url = reverse('account:subtype:detail-update', args=[subtype.pk])
+
+        resolver = resolve(url)
+
+        assert resolver.app_name == 'account:subtype'
+        assert resolver.url_name == 'detail-update'
+        assert resolver.view_name == 'account:subtype:detail-update'
+        assert resolver.namespace == 'account:subtype'
+        assert resolver.func.__name__ == 'AccountSubTypeRetrieveUpdateAPIView'
