a427eaab9e7210938291369e8e7f79a29aad24df
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-25 22:19:01 +0600

    Add test_username_field for User model test

--- user/tests/unit/test_models.py
+++ user/tests/unit/test_models.py
@@ -11,3 +11,14 @@ class TestUser:
         assert not User.REQUIRED_FIELDS
         assert User._meta.verbose_name == 'user'
         assert User._meta.verbose_name_plural == 'users'
+
+    def test_username_field(self):
+        user = User()
+        field = user._meta.get_field('username')
+        assert field.verbose_name == 'user name'
+        assert field.max_length == 128
+        assert field.blank
+        assert field.null
+        assert not field.has_default()
+        assert not field.hidden
+        assert not field.unique
