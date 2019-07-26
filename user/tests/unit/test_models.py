20050dac21f16792edc2d8431315235784e76c67
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-26 11:15:29 +0600

    Add test profile picture field for User

--- user/tests/unit/test_models.py
+++ user/tests/unit/test_models.py
@@ -93,3 +93,17 @@ class TestUser:
         assert not field.default
         assert not field.hidden
         assert not field.unique
+
+    def test_profile_picture_field(self):
+        user = User()
+        field = user._meta.get_field('profile_picture')
+        assert field.name == 'profile_picture'
+        assert field.get_attname() == 'profile_picture'
+        assert field.verbose_name == 'profile picture'
+        assert field.blank
+        assert field.null
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
+        assert field.upload_to == 'profile_picture/'
