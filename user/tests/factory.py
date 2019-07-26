5005478aca98d5ec26f277c64d1e1dbae153fe85
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-26 17:32:45 +0600

    Add UserFactory for user app

--- user/tests/factory.py
+++ user/tests/factory.py
@@ -1,13 +1,17 @@
-"""
-class ImageFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Image
+from faker import Faker
+from factory.django import DjangoModelFactory, ImageField
+from django.contrib.auth import get_user_model
 
-    image = factory.LazyAttribute(
-            lambda _: ContentFile(
-                factory.django.ImageField()._make_data(
-                    {'width': 1024, 'height': 768}
-                ), 'example.jpg'
-            )
-        )
-"""
+User = get_user_model()
+fake = Faker()
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = fake.name()
+    first_name = fake.name()
+    last_name = fake.name()
+    email = fake.email()
+    profile_picture = ImageField()
