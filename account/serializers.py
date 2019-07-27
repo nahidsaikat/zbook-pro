282ab0ce00f97c543d1c00ee97f7146dff27f5f5
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-27 09:10:22 +0600

    Account serializer added

--- account/serializers.py
+++ account/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import AccountSubType
+from .models import AccountSubType, Account
 
 
 class AccountSubTypeSerializer(serializers.ModelSerializer):
     """TODO: don't use ModelSerializer, use normal Serializer"""
     class Meta:
         model = AccountSubType
+        fields = '__all__'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    """TODO: don't use ModelSerializer, use normal Serializer"""
+    class Meta:
+        model = Account
         fields = '__all__'
