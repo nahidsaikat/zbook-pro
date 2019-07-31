bd3fb5b97d2b97da868eafccea0d122cda059e62
nahidsaikat <nahidsaikatft40@gmail.com>
2019-07-31 23:39:42 +0600

    Make email field lazzy attribute to have new email when ever an instance is created in UserFactory

--- user/tests/factory.py
+++ user/tests/factory.py
@@ -1,3 +1,4 @@
+import factory
 from faker import Faker
 from factory.django import DjangoModelFactory, ImageField
 from django.contrib.auth import get_user_model
@@ -9,9 +10,10 @@ fake = Faker()
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
+        django_get_or_create = ('email',)
 
     username = fake.name()
     first_name = fake.name()
     last_name = fake.name()
-    email = fake.email()
+    email = factory.LazyAttribute(lambda obj: f'{fake.email()}')
     profile_picture = ImageField()
