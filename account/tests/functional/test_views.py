a665539d1713bd548a90052736431d7c635f44e3
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-01 17:26:25 +0600

    Add test create code functional test for account

--- account/tests/functional/test_views.py
+++ account/tests/functional/test_views.py
@@ -163,3 +163,13 @@ class TestAccountListCreateAPIView:
         response = auth_client.post(self.url, data)
 
         assert response.status_code == 400
+
+    def test_create_code(self, auth_client, user):
+        data = factory.build(dict, FACTORY_CLASS=AccountFactory, created_by=user.pk)
+        data['sub_type'] = data['sub_type'].pk
+
+        del data['code']
+
+        response = auth_client.post(self.url, data)
+
+        assert response.status_code == 400
