371ddbb42539f7c51f10dc0d9ba8183021c5674c
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-02 11:25:44 +0600

    Add Base Views in system app

--- system/views.py
+++ system/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, CreateAPIView, RetrieveUpdateAPIView, \
+    RetrieveUpdateDestroyAPIView, UpdateAPIView
 
-# Create your views here.
+
+class BaseListCreateAPIView(ListCreateAPIView):
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        serializer.save(created_by=self.request.user)
+
+
+class BaseCreateAPIView(CreateAPIView):
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        serializer.save(created_by=self.request.user)
+
+
+class BaseRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+
+    def perform_update(self, serializer):
+        super().perform_update(serializer)
+        serializer.save(created_by=self.request.user)
+
+
+class BaseRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+
+    def perform_update(self, serializer):
+        super().perform_update(serializer)
+        serializer.save(created_by=self.request.user)
+
+
+class BaseUpdateAPIView(UpdateAPIView):
+
+    def perform_update(self, serializer):
+        super().perform_update(serializer)
+        serializer.save(created_by=self.request.user)
