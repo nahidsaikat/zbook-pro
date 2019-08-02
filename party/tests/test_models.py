b739fa523e88718fab1c27dc1988b19d3c5c9db2
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-02 21:48:27 +0600

    Add test_code_field for party sub type model

--- party/tests/test_models.py
+++ party/tests/test_models.py
@@ -18,3 +18,18 @@ class TestPartySubType:
         assert not field.hidden
         assert not field.unique
 
+    def test_code_field(self):
+        sub_type = PartySubType()
+        field = sub_type._meta.get_field('code')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'code'
+        assert field.max_length == 128
+        assert field.editable
+        assert field.blank
+        assert not field.null
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
+
