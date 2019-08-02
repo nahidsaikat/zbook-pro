74edd77c95ec6963593fba5ce719fc9ef0040910
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-02 21:49:53 +0600

    Add test_label_field for party sub type model

--- party/tests/test_models.py
+++ party/tests/test_models.py
@@ -33,3 +33,17 @@ class TestPartySubType:
         assert not field.hidden
         assert not field.unique
 
+    def test_label_field(self):
+        sub_type = PartySubType()
+        field = sub_type._meta.get_field('label')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'label'
+        assert field.max_length == 128
+        assert field.editable
+        assert field.blank
+        assert not field.null
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
