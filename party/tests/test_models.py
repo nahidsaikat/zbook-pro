aa2ed7baab3a1d163e1848298a755cbf5fc62d0e
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-07 09:29:56 +0600

    Add test_name_field for party model

--- party/tests/test_models.py
+++ party/tests/test_models.py
@@ -122,3 +122,21 @@ class TestPartySubType:
     def test_str(self, user):
         sub_type = PartySubType.objects.create(name=fake.name(), created_by=user)
         assert str(sub_type) == sub_type.name
+
+
+class TestParty:
+
+    def test_name_field(self):
+        sub_type = PartySubType()
+        field = sub_type._meta.get_field('name')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'name'
+        assert field.max_length == 128
+        assert field.editable
+        assert not field.blank
+        assert not field.null
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
