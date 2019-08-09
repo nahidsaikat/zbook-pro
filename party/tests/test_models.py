cf956835d1ee610427b0fe288fd23b62bc66dbd8
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-09 09:45:06 +0600

    Add test_bin_field for party model

--- party/tests/test_models.py
+++ party/tests/test_models.py
@@ -336,3 +336,18 @@ class TestParty:
         assert field.default.__name__ == 'NOT_PROVIDED'
         assert not field.hidden
         assert not field.unique
+
+    def test_bin_field(self):
+        party = Party()
+        field = party._meta.get_field('bin')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'bin'
+        assert field.max_length == 64
+        assert field.editable
+        assert field.null
+        assert field.blank
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
