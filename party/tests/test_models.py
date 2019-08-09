b5498b0daa53cc643b31822609f2929a5433d0b8
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-09 09:42:51 +0600

    Add test_passport_field for party model

--- party/tests/test_models.py
+++ party/tests/test_models.py
@@ -306,3 +306,18 @@ class TestParty:
         assert field.default.__name__ == 'NOT_PROVIDED'
         assert not field.hidden
         assert not field.unique
+
+    def test_passport_field(self):
+        party = Party()
+        field = party._meta.get_field('passport')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'passport'
+        assert field.max_length == 64
+        assert field.editable
+        assert field.null
+        assert field.blank
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
