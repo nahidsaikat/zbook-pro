085b6a5aa1b50c64f90ca0a796bbe3962238c5d8
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-09 09:46:04 +0600

    Add test_vat_reg_no_field for party model

--- party/tests/test_models.py
+++ party/tests/test_models.py
@@ -351,3 +351,18 @@ class TestParty:
         assert field.default.__name__ == 'NOT_PROVIDED'
         assert not field.hidden
         assert not field.unique
+
+    def test_vat_reg_no_field(self):
+        party = Party()
+        field = party._meta.get_field('vat_reg_no')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'vat reg no'
+        assert field.max_length == 64
+        assert field.editable
+        assert field.null
+        assert field.blank
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
