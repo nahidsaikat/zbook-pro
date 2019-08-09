31bdee4b50fe6f2059c281bb965b0a728de11a52
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-09 09:38:57 +0600

    Add test_bank_account_name_field for party model

--- party/tests/test_models.py
+++ party/tests/test_models.py
@@ -276,3 +276,18 @@ class TestParty:
         assert field.default.__name__ == 'NOT_PROVIDED'
         assert not field.hidden
         assert not field.unique
+
+    def test_bank_account_name_field(self):
+        party = Party()
+        field = party._meta.get_field('bank_account_name')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'bank account name'
+        assert field.max_length == 64
+        assert field.editable
+        assert field.null
+        assert field.blank
+        assert not field.has_default()
+        assert field.default.__name__ == 'NOT_PROVIDED'
+        assert not field.hidden
+        assert not field.unique
