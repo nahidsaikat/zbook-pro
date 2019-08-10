eab6183903385d88516a0fb3be914eacec1956e8
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-10 13:02:40 +0600

    Add test_create_code_auto_generated in TestPartySubTypeSerializer for party suh type model

--- party/tests/test_serializers.py
+++ party/tests/test_serializers.py
@@ -42,3 +42,21 @@ class TestPartySubTypeSerializer:
 
         serializer = PartySubTypeSerializer(data=data)
         assert not serializer.is_valid()
+
+    def test_create_code_auto_generated(self, user):
+        name = fake.name()
+        data = factory.build(dict, FACTORY_CLASS=PartySubTypeFactory, name=name, created_by=user.pk)
+
+        serializer = PartySubTypeSerializer(data=data)
+        serializer.is_valid()
+
+        assert serializer.validated_data.get('name') == name
+
+        serializer.save()
+
+        query = PartySubType.objects.all()
+        sub_type = query.first()
+
+        assert query.count() == 1
+        assert sub_type.name == name
+        assert sub_type.code == str(name).strip().lower().replace(' ', '-')
