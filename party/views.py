0edb9e4dc412e3cb3046c6203ec2e5c916b1d3a2
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-11 21:19:25 +0600

    Add PartySubTypeListCreateAPIView for party app

--- party/views.py
+++ party/views.py
@@ -1,5 +1,11 @@
+from rest_framework.permissions import IsAuthenticated
+
 from system.views import BaseListCreateAPIView
+from .models import PartySubType
+from .serializers import PartySubTypeSerializer
 
 
 class PartySubTypeListCreateAPIView(BaseListCreateAPIView):
-    pass
+    queryset = PartySubType.objects.all().order_by('-id')
+    serializer_class = PartySubTypeSerializer
+    permission_classes = [IsAuthenticated]
