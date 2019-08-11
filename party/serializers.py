372d8db9f9304688daab48007f44f368a2319217
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-11 19:38:31 +0600

    Add serializer class for Vendor model of party app

--- party/serializers.py
+++ party/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import PartySubType, Customer
+from .models import PartySubType, Customer, Vendor
 
 
 class PartySubTypeSerializer(serializers.ModelSerializer):
@@ -15,4 +15,11 @@ class CustomerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Customer
+        fields = '__all__'
+
+
+class VendorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Vendor
         fields = '__all__'
