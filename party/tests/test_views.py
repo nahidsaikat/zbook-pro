df06140ff75f9b8e82bdb5a27e5f8ac873ad1142
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-12 10:47:43 +0600

    Add test_create_unauthorize in TestCustomerListCreateAPIView of party app

--- party/tests/test_views.py
+++ party/tests/test_views.py
@@ -190,3 +190,10 @@ class TestCustomerListCreateAPIView:
         response = auth_client.post(self.url, data)
 
         assert response.status_code == 400
+
+    def test_create_unauthorize(self, client, user):
+        data = factory.build(dict, FACTORY_CLASS=CustomerFactory)
+
+        response = client.post(self.url, data)
+
+        assert response.status_code == 401
