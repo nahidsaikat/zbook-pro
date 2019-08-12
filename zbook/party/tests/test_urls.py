61c6e2addb614121c284fc9df7a40db5c3dd1cca
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-12 20:23:46 +0600

    Add test_list_create_url_resolve in TestAccountSubTypeUrls of party app

--- zbook/party/tests/test_urls.py
+++ zbook/party/tests/test_urls.py
@@ -1,4 +1,4 @@
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 
 class TestAccountSubTypeUrls:
@@ -6,3 +6,14 @@ class TestAccountSubTypeUrls:
     def test_list_create_url_reverse(self):
         url = reverse('party:subtype:list-create')
         assert url == '/api/v1/party/subtype/'
+
+    def test_list_create_url_resolve(self):
+        url = reverse('party:subtype:list-create')
+
+        resolver = resolve(url)
+
+        assert resolver.app_name == 'party:subtype'
+        assert resolver.url_name == 'list-create'
+        assert resolver.view_name == 'party:subtype:list-create'
+        assert resolver.namespace == 'party:subtype'
+        assert resolver.func.__name__ == 'PartySubTypeListCreateAPIView'
