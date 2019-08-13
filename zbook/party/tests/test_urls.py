6fd2c5fe2cea02c816bd4ea9373823f083ff4277
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-13 15:45:51 +0600

    Add test_detail_update_url_resolve in TestVendorUrls of party app

--- zbook/party/tests/test_urls.py
+++ zbook/party/tests/test_urls.py
@@ -93,3 +93,15 @@ class TestVendorUrls:
         vendor = VendorFactory(created_by=user)
         url = reverse('party:vendor:detail-update', args=[vendor.pk])
         assert url == f'/api/v1/party/vendor/{vendor.pk}/'
+
+    def test_detail_update_url_resolve(self, user):
+        vendor = VendorFactory(created_by=user)
+        url = reverse('party:vendor:detail-update', args=[vendor.pk])
+
+        resolver = resolve(url)
+
+        assert resolver.app_name == 'party:vendor'
+        assert resolver.url_name == 'detail-update'
+        assert resolver.view_name == 'party:vendor:detail-update'
+        assert resolver.namespace == 'party:vendor'
+        assert resolver.func.__name__ == 'VendorRetrieveUpdateAPIView'
