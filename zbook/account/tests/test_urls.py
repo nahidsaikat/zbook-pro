b91ec8fbdbce77a71c6d3580284f8b152c0c08d5
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-13 15:27:00 +0600

    Add test_list_create_url_resolve in TestAccountUrls of account app

--- zbook/account/tests/test_urls.py
+++ zbook/account/tests/test_urls.py
@@ -42,3 +42,14 @@ class TestAccountUrls:
     def test_list_create_url_reverse(self):
         url = reverse('account:list-create')
         assert url == '/api/v1/account/'
+
+    def test_list_create_url_resolve(self):
+        url = reverse('account:list-create')
+
+        resolver = resolve(url)
+
+        assert resolver.app_name == 'account'
+        assert resolver.url_name == 'list-create'
+        assert resolver.view_name == 'account:list-create'
+        assert resolver.namespace == 'account'
+        assert resolver.func.__name__ == 'AccountListCreateAPIView'
