f3381bf203dc95489e49e5640325a3689b9c97d4
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-13 17:55:24 +0600

    Add test_type_field in TestVoucherSubType of voucher app

--- zbook/voucher/tests/test_models.py
+++ zbook/voucher/tests/test_models.py
@@ -1,4 +1,5 @@
 from ..models import VoucherSubType
+from ..choices import VoucherType
 
 
 class TestVoucherSubType:
@@ -14,5 +15,20 @@ class TestVoucherSubType:
         assert not field.blank
         assert not field.null
         assert not field.has_default()
+        assert not field.hidden
+        assert not field.unique
+
+    def test_type_field(self):
+        sub_type = VoucherSubType()
+        field = sub_type._meta.get_field('type')
+
+        assert field.__class__.__name__ == 'IntegerField'
+        assert field.verbose_name == 'type'
+        assert field.editable
+        assert not field.blank
+        assert not field.null
+        assert field.has_default()
+        assert field.default == VoucherType.Receive
+        assert field.choices == VoucherType.choices
         assert not field.hidden
         assert not field.unique
