dae9633f1a92f5e250575f10071e02df3a3a6b9b
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-13 17:56:44 +0600

    Add test_prefix_field in TestVoucherSubType of voucher app

--- zbook/voucher/tests/test_models.py
+++ zbook/voucher/tests/test_models.py
@@ -32,3 +32,17 @@ class TestVoucherSubType:
         assert field.choices == VoucherType.choices
         assert not field.hidden
         assert not field.unique
+
+    def test_prefix_field(self, db):
+        sub_type = VoucherSubType()
+        field = sub_type._meta.get_field('prefix')
+
+        assert field.__class__.__name__ == 'CharField'
+        assert field.verbose_name == 'prefix'
+        assert field.max_length == 64
+        assert field.editable
+        assert not field.blank
+        assert not field.null
+        assert not field.has_default()
+        assert not field.hidden
+        assert not field.unique
