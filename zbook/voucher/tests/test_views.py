d1d7dc7fb6880f54870f8d861487f3916e221544
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-14 19:26:13 +0600

    Add test_create in TestPartySubTypeListCreateAPIView of voucher app

---
+++ zbook/voucher/tests/test_views.py
@@ -0,0 +1,29 @@
+import factory
+from faker import Faker
+from factory.fuzzy import FuzzyChoice
+from django.urls import reverse
+
+from zbook.voucher.choices import VoucherType
+from .factory import VoucherSubTypeFactory
+
+fake = Faker()
+
+
+class TestPartySubTypeListCreateAPIView:
+    url = reverse('voucher:subtype:list-create')
+
+    def test_create(self, auth_client, user):
+        name = fake.name()
+        _type = FuzzyChoice(choices=VoucherType.values.keys()).fuzz()
+        prefix = fake.name()
+        data = factory.build(dict, FACTORY_CLASS=VoucherSubTypeFactory, name=name, type=_type, prefix=prefix)
+        data['debit_account'] = data['debit_account'].pk
+        data['credit_account'] = data['credit_account'].pk
+
+        response = auth_client.post(self.url, data)
+
+        assert response.status_code == 201
+        assert response.data.get('name') == name
+        assert response.data.get('prefix') == prefix
+        assert response.data.get('type') == _type
+        assert response.data.get('created_by') == user.pk
