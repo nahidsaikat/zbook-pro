ed87f31aa9e2a5b1d07a82b1c85bebf35c403ae5
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-14 13:05:31 +0600

    Add test_create in TestVoucherSubTypeSerializer of voucher app

---
+++ zbook/voucher/tests/test_serializers.py
@@ -0,0 +1,33 @@
+import factory
+from faker import Faker
+
+from .factory import VoucherSubTypeFactory
+from ..models import VoucherSubType
+from ..serializers import VoucherSubTypeSerializer
+
+fake = Faker()
+
+
+class TestVoucherSubTypeSerailizer:
+
+    def test_create(self, user):
+        name = fake.name()
+        prefix = fake.name()
+        data = factory.build(dict, FACTORY_CLASS=VoucherSubTypeFactory, name=name, prefix=prefix, created_by=user.pk)
+        data['debit_account'] = data['debit_account'].pk
+        data['credit_account'] = data['credit_account'].pk
+
+        serializer = VoucherSubTypeSerializer(data=data)
+        serializer.is_valid()
+
+        assert serializer.validated_data.get('name') == name
+        assert serializer.validated_data.get('prefix') == prefix
+
+        serializer.save()
+
+        query = VoucherSubType.objects.all()
+        sub_type = query.first()
+
+        assert query.count() == 1
+        assert sub_type.name == name
+        assert sub_type.prefix == prefix
