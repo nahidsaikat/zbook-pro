2ce937070ea38f3f4cb202f65320ff42bed20ebd
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-14 13:03:20 +0600

    Add type field in VoucherSubTypeFactory

--- zbook/voucher/tests/factory.py
+++ zbook/voucher/tests/factory.py
@@ -1,10 +1,12 @@
 import factory
 from faker import Faker
+from factory.fuzzy import FuzzyChoice
 from factory.django import DjangoModelFactory
 
 from zbook.account.tests.factory import AccountFactory
 from zbook.user.tests.factory import UserFactory
 from ..models import VoucherSubType
+from ..choices import VoucherType
 
 fake = Faker()
 
@@ -13,6 +15,7 @@ class VoucherSubTypeFactory(DjangoModelFactory):
     class Meta:
         model = VoucherSubType
 
+    type = FuzzyChoice(choices=VoucherType.values.keys())
     name = fake.name()
     prefix = fake.name()
     debit_account = factory.SubFactory(AccountFactory)
