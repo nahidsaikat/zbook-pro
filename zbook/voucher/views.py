7f337fbaf5d92c0ecce120d97ddf8d50c154053f
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-14 19:24:59 +0600

    add VoucherSubTypeListCreateAPIView class for voucher app

--- zbook/voucher/views.py
+++ zbook/voucher/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from zbook.system.views import BaseListCreateAPIView
+from .models import VoucherSubType
+from .serializers import VoucherSubTypeSerializer
+
+
+class VoucherSubTypeListCreateAPIView(BaseListCreateAPIView):
+    queryset = VoucherSubType.objects.all().order_by('-id')
+    serializer_class = VoucherSubTypeSerializer
+    permission_classes = [IsAuthenticated]
