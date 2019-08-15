b326068419c5df78a0dfc5894f887c90dc4d2dcc
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-15 14:22:14 +0600

    Add VoucherFactory class for voucher app

--- zbook/voucher/tests/factory.py
+++ zbook/voucher/tests/factory.py
@@ -2,10 +2,11 @@ import factory
 from faker import Faker
 from factory.fuzzy import FuzzyChoice
 from factory.django import DjangoModelFactory
+from django.db.models.signals import post_save
 
-from zbook.account.tests.factory import AccountFactory
 from zbook.user.tests.factory import UserFactory
-from ..models import VoucherSubType
+from zbook.account.tests.factory import AccountFactory
+from ..models import VoucherSubType, Voucher
 from ..choices import VoucherType
 
 fake = Faker()
@@ -20,3 +21,29 @@ class VoucherSubTypeFactory(DjangoModelFactory):
     prefix = fake.name()
     debit_account = factory.SubFactory(AccountFactory)
     credit_account = factory.SubFactory(AccountFactory)
+    created_by = factory.SubFactory(UserFactory)
+
+
+@factory.django.mute_signals(post_save)
+class VoucherFactory(DjangoModelFactory):
+    class Meta:
+        model = Voucher
+
+    voucher_number = fake.name()
+    type = FuzzyChoice(choices=VoucherType.values.keys())
+    sub_type = factory.SubFactory(VoucherSubTypeFactory)
+    amount = fake.random_number(digits=5)
+    description = fake.sentence()
+    created_by = factory.SubFactory(UserFactory)
+
+    @factory.post_generation
+    def accounts(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for account in extracted:
+                self.accounts.add(account)
+        else:
+            self.accounts.add(AccountFactory())
+            self.accounts.add(AccountFactory())
