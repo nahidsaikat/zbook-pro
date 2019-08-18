0a4ed57be7c4857adf792729b15c6fdcda25cea1
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-18 16:44:22 +0600

    Add VoucherSerializer for voucher app

--- zbook/voucher/serializers.py
+++ zbook/voucher/serializers.py
@@ -1,9 +1,16 @@
 from zbook.system.serializers import BaseSerializer
-from .models import VoucherSubType
+from .models import VoucherSubType, Voucher
 
 
 class VoucherSubTypeSerializer(BaseSerializer):
 
     class Meta:
         model = VoucherSubType
+        fields = '__all__'
+
+
+class VoucherSerializer(BaseSerializer):
+
+    class Meta:
+        model = Voucher
         fields = '__all__'
