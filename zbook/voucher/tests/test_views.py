d9a84ab2283f5faafd2db613b93ea3281c7f07b3
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-19 01:22:21 +0600

    Add test_create_unauthorize in TestVoucherListCreateAPIView of voucher app

--- zbook/voucher/tests/test_views.py
+++ zbook/voucher/tests/test_views.py
@@ -218,3 +218,13 @@ class TestVoucherListCreateAPIView:
         response = auth_client.post(self.url, data)
 
         assert response.status_code == 400
+
+    def test_create_unauthorize(self, client, user, sub_type, debit_account, credit_account):
+        voucher_number = fake.name()
+        _type = FuzzyChoice(choices=VoucherType.values.keys()).fuzz()
+        data = factory.build(dict, FACTORY_CLASS=VoucherFactory, created_by=user.pk, type=_type, sub_type=sub_type.pk,
+                             voucher_number=voucher_number, accounts=[debit_account.pk, credit_account.pk])
+
+        response = client.post(self.url, data)
+
+        assert response.status_code == 401
