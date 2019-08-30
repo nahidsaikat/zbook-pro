c67c5ca384c0ec31e24974a23c1e0c98e2ffd432
nahidsaikat <nahidsaikatft40@gmail.com>
2019-08-30 12:19:44 +0600

    Add test_update_inactive in TestVoucherRetrieveUpdateAPIView of voucher app

--- zbook/voucher/tests/test_views.py
+++ zbook/voucher/tests/test_views.py
@@ -270,3 +270,15 @@ class TestVoucherRetrieveUpdateAPIView:
         assert response.data.get('voucher_number') == voucher_number
         assert response.data.get('sub_type') == sub_type.pk
         assert response.data.get('type') == sub_type.type
+
+    def test_update_inactive(self, auth_client, user, sub_type, debit_account, credit_account):
+        voucher = VoucherFactory(accounts=[debit_account, credit_account], created_by=user)
+        voucher_number = fake.name()
+        data = factory.build(dict, FACTORY_CLASS=VoucherFactory, voucher_number=voucher_number, sub_type=sub_type.pk,
+                             type=sub_type.type, created_by=user.pk, inactive=True)
+
+        url = reverse('voucher:subtype:detail-update', args=[voucher.pk])
+        response = auth_client.patch(url, data)
+
+        assert response.status_code == 200
+        assert response.data.get('inactive') == True
