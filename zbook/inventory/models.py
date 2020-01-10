1a50221c744742b96b96c7f3d97bd6bd19cf2040
nahidsaikat <nahidsaikatft40@gmail.com>
2020-01-10 19:33:17 +0600

    Add inventory model in inventory app

--- zbook/inventory/models.py
+++ zbook/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from zbook.system.models import BaseModel
+from zbook.account.models import Account
+
+
+class Inventory(BaseModel):
+    name = models.CharField(max_length=64)
+    code = models.CharField(max_length=16, null=True, blank=True)
+    address = models.TextField(default='', null=True, blank=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse('account:subtype:detail-update', args=[self.pk])
